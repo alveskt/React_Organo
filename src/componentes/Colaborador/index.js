021ee1f3a84f1d23244f7f3e07e49bbaf7a49c25
alveskt <asgustavo95@gmail.com>
2022-08-05 16:49:15 -0300

    inserindo a img padrao

--- src/componentes/Colaborador/index.js
+++ src/componentes/Colaborador/index.js
@@ -1,23 +1,40 @@
 import './Colaborador.css'
+import { useState } from 'react';
+import perfil from './perfil.png'
 import {CgClose} from 'react-icons/cg'
 
 
 const Colaborador = ({nome, img, cargo, matricula, corSec, corPrim, delet}) => {
     
+    const [newImage, setNewImage] = useState(true);
+
+    const verificaImagem = (img) => {
+        let imagem = new Image();
+        imagem.src = img;
+        imagem.onerror = () => {
+            setNewImage(false);
+        }    
+    }
+    verificaImagem(img);
+
     return (
         <div className='colaborador'>
             <div className='cabecalho' style={{backgroundColor: corPrim}}>
-                <img src={img} alt='Imagem de Perfil'/>
-                
+                <img src={newImage ? img : perfil} alt='Imagem de Perfil'/>
+                <div className='botao-excluir__container'>
+                    <button className='botao-excluir' onClick={() => delet(matricula)} style={ {backgroundColor: corSec}}>
+                        <CgClose />
+                    </button>
+                </div>
             </div>
             
             <div className='rodape'>
                 <h4 style={{ color: corSec}}>{nome}</h4>
                 <h5>{cargo}</h5>
                 <h5>{matricula}</h5>
-                <button className='botao-excluir' onClick={() => delet(matricula)} style={ {backgroundColor: corSec}}>
-                    <CgClose />
-                </button>
+                
+                
+                
             </div>  
         </div>
     )
