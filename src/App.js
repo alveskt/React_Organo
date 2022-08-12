5f27707c10fffa8e5eb8d805926e56d8314bc042
alveskt <asgustavo95@gmail.com>
2022-08-12 09:44:58 -0300

    Revert "adicionando o db.json"
    This reverts commit 09c6c3710d2cd92b81229504d6272ebc2a0969e4.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import Banner from './componentes/Banner';
 import Formulario from './componentes/Formulario';
 import Rodape from './componentes/Rodape';
@@ -6,21 +6,43 @@ import Time from './componentes/Time';
 
 function App() {
 
-  const [times, setTimes] = useState([]);
-
-  useEffect(() => {
-    fetch('http://localhost:5000/times', {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    })
-      .then((resp) => resp.json())
-      .then((data) => {
-        setTimes(data)
-      })
-      .catch((err) => console.log(err))
-  }, [])
+  const times = [
+    {
+      nome: 'Programação',
+      corPrimaria: '#57C278',
+      corSecundaria: '#d9F7E9'
+    },
+    {
+      nome: 'Front-End',
+      corPrimaria: '#82CFFA',
+      corSecundaria: '#E8F8FF'
+    },
+    {
+      nome: 'Data Science',
+      corPrimaria: '#A6D157',
+      corSecundaria: 'F0F8E2'
+    },
+    {
+      nome: 'Devops',
+      corPrimaria: '#E06B69',
+      corSecundaria: '#FDE7E8'
+    },
+    {
+      nome: 'UX e Design',
+      corPrimaria: '#DB6EBF',
+      corSecundaria: '#FAE9F5'
+    },
+    {
+      nome: 'Mobile',
+      corPrimaria: '#FFBA05',
+      corSecundaria: '#FFF5D9'
+    },
+    {
+      nome: 'Inovação e Gestão',
+      corPrimaria: '#FF8A29',
+      corSecundaria: '#FFEEDF'
+    },
+  ];
 
   const [colaboradores, setColaboradores] = useState([]);
 
